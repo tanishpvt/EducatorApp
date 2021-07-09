8e1107dea4c2ccc85d1603ea60ee6202e93e4e19
Tanish Mohanty <tanish0911@gmail.com>
2021-07-10 00:20:18 +0530

    Feature Updates
    Added comment section,edit profile,follow and followers
    bug fixes

--- app/src/main/java/com/example/vit/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/vit/Fragment/HomeFragment.java
@@ -10,7 +10,10 @@ import android.widget.ProgressBar;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.vit.Adapter.PostAdapter;
+import com.example.vit.Model.Post;
 import com.example.vit.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -26,14 +29,95 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private PostAdapter postAdapter;
+    private List<Post> postLists;
+    private RecyclerView recyclerView_story;
+    private List<String> followingList;
+
+    ProgressBar progressBar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.fragment_home, container, false);
 
+        recyclerView =view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        postLists = new ArrayList<>();
+        postAdapter = new PostAdapter(getContext(),postLists);
+        recyclerView.setAdapter(postAdapter);
+
+        recyclerView_story = view.findViewById(R.id.recycler_view_story);
+        recyclerView_story.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager1 = new LinearLayoutManager(getContext(),
+                LinearLayoutManager.HORIZONTAL, false);
+        recyclerView_story.setLayoutManager(linearLayoutManager1);
+        progressBar = view.findViewById(R.id.progress_circular);
+
+        checkFollowing();
 
         return view;
+    }
+    private  void checkFollowing(){
+        followingList = new ArrayList<>();
+        DatabaseReference reference =FirebaseDatabase.getInstance().getReference("Follow")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child("following");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                followingList.clear();
+                for (DataSnapshot snapshot :dataSnapshot.getChildren()){
+                    followingList.add(snapshot.getKey());
+                }
+                readposts();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+
+
+    private void readposts(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("posts");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                postLists.clear();
+
+                for (DataSnapshot snapshot:dataSnapshot.getChildren()){
+                    Post post = snapshot.getValue(Post.class);
+                    for (String id : followingList){
+
+                        assert post != null;
+                        if (post.getPublisher().equals(id)){
+                            //   Toast.makeText(getContext(), ""+post.getPublisher(), Toast.LENGTH_SHORT).show();
+                            postLists.add(post);
+                        }
+                    }
+                }
+
+                postAdapter.notifyDataSetChanged();
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
